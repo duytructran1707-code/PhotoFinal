8ba4ad1b18ba7d1d0ce15fdf57667097021055ce
duytructran1707-code <duytructran1707@gmail.com>
2026-04-17 01:51:26 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || 3000;
 // middlewares
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+// apis
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello from server!' });
+});
+app.use('/api/admin', require('./api/admin.js'));
+app.use('/api/customer', require('./api/customer.js'));
+
 app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
 app.get('admin/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
@@ -16,12 +24,7 @@ app.use('/', express.static(path.resolve(__dirname, '../client-customer/build'))
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
 });
-// apis
-app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from server!' });
-});
-app.use('/api/admin', require('./api/admin.js'));
-app.use('/api/customer', require('./api/customer.js'));
+
 // start server
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
