03bef893387018af642277018946381201613c2d
duytructran1707-code <duytructran1707@gmail.com>
2026-04-17 01:46:11 +0700

    Serve static files for admin and customer clients

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,7 +8,14 @@ const PORT = process.env.PORT || 3000;
 // middlewares
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
+});
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
 // apis
 app.get('/hello', (req, res) => {
   res.json({ message: 'Hello from server!' });
